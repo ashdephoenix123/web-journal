4bb132ed7e0655db65cfc9f832ef17b9c73a9591
akash-appycodes <akash@appycodes.com>
2024-10-22 18:56:53 +0530

    added some tools

--- sanity.config.js
+++ sanity.config.js
@@ -1,28 +1,43 @@
-'use client'
+"use client";
 
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\studio\[[...tool]]\page.jsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './src/sanity/env'
-import {schema} from './src/sanity/schemaTypes'
-import {structure} from './src/sanity/structure'
+import { apiVersion, dataset, projectId } from "./src/sanity/env";
+import { schema } from "./src/sanity/schemaTypes";
+import { structure } from "./src/sanity/structure";
+import { media } from "sanity-plugin-media";
+import { RocketIcon } from "@sanity/icons";
 
 export default defineConfig({
-  basePath: '/studio',
+  basePath: "/studio",
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({structure}),
+    structureTool({ structure }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({ defaultApiVersion: apiVersion }),
+    media(),
   ],
-})
+  tools: (prev, context) => {
+    console.log(context); // logs { getClient, currentUser, schema, projectId, dataset}
+    return [
+      {
+        name: "my-tool",
+        title: "My super-cool tool",
+        icon: RocketIcon,
+        component: (props) => <div>I am a tool, albeit not a useful one</div>,
+      },
+      ...prev, // remember to include previous values
+    ];
+  },
+});
--- src/sanity/schemaTypes/postType.js
+++ src/sanity/schemaTypes/postType.js
@@ -1,65 +1,94 @@
-import {DocumentTextIcon} from '@sanity/icons'
-import {defineArrayMember, defineField, defineType} from 'sanity'
+import { DocumentTextIcon } from "@sanity/icons";
+import { defineArrayMember, defineField, defineType } from "sanity";
 
 export const postType = defineType({
-  name: 'post',
-  title: 'Post',
-  type: 'document',
+  name: "post",
+  title: "Post",
+  type: "document",
   icon: DocumentTextIcon,
   fields: [
     defineField({
-      name: 'title',
-      type: 'string',
+      name: "title",
+      type: "string",
     }),
     defineField({
-      name: 'slug',
-      type: 'slug',
+      name: "subTitle",
+      title: "Sub Title",
+      type: "string",
+      hidden: ({ document }) => !document?.title,
+    }),
+    defineField({
+      name: "link",
+      type: "object",
+      title: "Link",
+      fields: [
+        {
+          name: "external",
+          type: "url",
+          title: "URL",
+          hidden: ({ parent, value }) => !value && parent?.internal,
+        },
+        {
+          name: "internal",
+          type: "reference",
+          to: [{ type: "author" }],
+          hidden: ({ parent, value }) => !value && parent?.external,
+        },
+      ],
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
       options: {
-        source: 'title',
+        source: "title",
+      },
+      readOnly: ({ currentUser }) => {
+        console.log("currentUser", currentUser);
+        return !currentUser.roles.find(({ name }) => name === "administrator");
       },
     }),
     defineField({
-      name: 'author',
-      type: 'reference',
-      to: {type: 'author'},
+      name: "author",
+      type: "reference",
+      to: { type: "author" },
     }),
     defineField({
-      name: 'mainImage',
-      type: 'image',
+      name: "mainImage",
+      type: "image",
       options: {
         hotspot: true,
       },
       fields: [
         {
-          name: 'alt',
-          type: 'string',
-          title: 'Alternative text',
-        }
-      ]
+          name: "alt",
+          type: "string",
+          title: "Alternative text",
+        },
+      ],
     }),
     defineField({
-      name: 'categories',
-      type: 'array',
-      of: [defineArrayMember({type: 'reference', to: {type: 'category'}})],
+      name: "categories",
+      type: "array",
+      of: [defineArrayMember({ type: "reference", to: { type: "category" } })],
     }),
     defineField({
-      name: 'publishedAt',
-      type: 'datetime',
+      name: "publishedAt",
+      type: "datetime",
     }),
     defineField({
-      name: 'body',
-      type: 'blockContent',
+      name: "body",
+      type: "blockContent",
     }),
   ],
   preview: {
     select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
     },
     prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
+      const { author } = selection;
+      return { ...selection, subtitle: author && `by ${author}` };
     },
   },
-})
+});
