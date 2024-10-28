483c614dbe670db0b0982e94d44eff06ac0fbc0c
ashdephoenix123 <akashsarki12345@gmail.com>
2024-10-28 08:02:16 +0530

    added default export in fonts

--- src/pages/fonts/fonts.js
+++ src/pages/fonts/fonts.js
@@ -1,6 +1,8 @@
 import { Libre_Baskerville } from "next/font/google";
 
-export const libre = Libre_Baskerville({
+const libre = Libre_Baskerville({
   weight: "400",
   subsets: ["latin"],
 });
+
+export default libre;
