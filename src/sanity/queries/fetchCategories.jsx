659f87b3b18eeb861fe32023bd8898ea2cf1b7f1
ashdephoenix123 <akashsarki12345@gmail.com>
2025-01-20 20:45:56 +0530

    getting updatedat from sanity for categories aswell

--- src/sanity/queries/fetchCategories.jsx
+++ src/sanity/queries/fetchCategories.jsx
@@ -2,7 +2,7 @@ import { client } from "../lib/client";
 
 export const fetchAllCategories = async () => {
   const categories = await client.fetch(
-    `*[_type == "category"  ] {"id":_id, "name": title, "slug": slug.current}`
+    `*[_type == "category"  ] {"id":_id, "name": title, "slug": slug.current, _updatedAt}`
   );
   return categories;
 };
